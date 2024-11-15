0ed7b3d356a7c890fe50c12c8db1e3e4bcc1ba21
1yavuz <1yavuzahmet@gmail.com>
2024-11-15 23:42:57 +0300

    tw friday completed

---
+++ frontend/teamwork-agendas/week-4/night-work/sceipt.js
@@ -0,0 +1,19 @@
+for (let index = 0; index < 10; index++) {
+    console.log("*".repeat(index));
+}
+
+function iterateString(inputString) {
+    for (let index = 0; index < inputString.length; index++) {
+        console.log(inputString[index]);
+        //write your code here 
+    }
+}
+let str = 'Hello World';
+iterateString(str)
+
+var start = 1;
+function setEnd() {
+    var end = 10;
+}
+setEnd();
+console.log(end);
