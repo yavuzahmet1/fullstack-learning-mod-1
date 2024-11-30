8c469e9b66920a090f37f2d627a83bf267eea7e4
1yavuz <1yavuzahmet@gmail.com>
2024-11-30 23:32:06 +0300

    coffe site completed

--- projects/coffee-shop/style-and-js/script.js
+++ projects/coffee-shop/style-and-js/script.js
@@ -1,5 +1,22 @@
 let search = document.querySelector(".search-box");
+let header = document.querySelector("header");
+let navbar = document.querySelector(".navbar");
 
 document.querySelector("#search-icon").onclick = () => {
     search.classList.toggle("active");
+    navbar.classList.remove("active");
 }
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+    search.classList.remove("active");
+}
+
+document.querySelector("#menu-icon").onclick = () => {
+    navbar.classList.toggle("active");
+    search.classList.remove("active");
+}
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0)
+})
