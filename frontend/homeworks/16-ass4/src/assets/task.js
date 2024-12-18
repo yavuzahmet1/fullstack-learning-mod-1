8345e474f1b1f7af25f8ad211f9de6bccfab3ef1
1yavuz <1yavuzahmet@gmail.com>
2024-12-18 23:05:38 +0300

    button id selected

--- frontend/homeworks/16-ass4/src/assets/task.js
+++ frontend/homeworks/16-ass4/src/assets/task.js
@@ -3,15 +3,15 @@ const aside = document.querySelector(".aside-container");
 const basketBtn = document.querySelector(".basket-btn");
 const cards = document.querySelector(".cards")
 const elecronicsBtn = document.querySelector(".elecronics-btn")
+const basketAside = document.querySelector(".basket-aside");
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
-const url = "https://anthonyfs.pythonanywhere.com/api/products/";
 
 window.addEventListener("load", hide(true));
 
-const getData = async (url) => {
-    console.log("first")
+const getData = async () => {
     try {
-        const res = await fetch(url);
+        const res = await fetch("https://anthonyfs.pythonanywhere.com/api/products/");
 
         if (!res.ok) {
 
@@ -19,7 +19,7 @@ const getData = async (url) => {
         }
         const data = await res.json()
         console.log(data)
-        // console.log(data[0].image)
+
         showToScreen(data)
     } catch (error) {
         console.log("Somethin went wrong")
@@ -29,9 +29,9 @@ getData()
 
 function showToScreen(getData) {
     getData.map(element => {
-        const { image, description, price, title, category } = element;
-        if (category) {
-            cards.innerHTML += `
+        const { id, image, description, price, title } = element;
+
+        cards.innerHTML += `
           <div class="card" style="width: 18rem;">
         <img class="card-img-top w-75" src="${image}" alt="Card image cap">
         <div class="card-body">
@@ -43,16 +43,59 @@ function showToScreen(getData) {
             <p class="price">${price}</p>
           </div>
           <div class="cart-btn d-flex justify-content-around p-2">
-            <a href="#" class="btn btn-danger">Add To Cart</a>
-            <a href="#" class="btn btn-primary">See Details</a>
+            <a href="#" onclick="addToCard(${element})" class="btn btn-danger">Add To Cart</a>
+            <a href="#" onclick="seeDetails()"  class="btn btn-primary">See Details</a>
           </div>
         </div>
 
         `
-        }
+
     });
 
 }
+
+const addToCard = (id) => {
+    console.log(id);
+    localStorage.setItem("data", JSON.stringify(id));
+    let selectedItem = id;
+    let search = basket.find(product => product.id === selectedItem.id);
+    // basketAside.innerHTML += `
+    // </div>
+    // <div class="cart-item">
+    //   <img width="100" src="./src/assets/computer.jpg" alt="" />
+    //   <div class="details">
+    //     <div class="title-price-x">
+    //       <h5 class="title-price">
+    //         <p>Macbook</p>
+    //       </h5>
+    //     </div>
+    //     <div class="buttons-cart">
+    //       <i onclick="decrement()" class="fa-solid fa-minus"></i>
+    //       <div id=${id} class="quantity"> 5</div>
+    //       <i onclick="decrement()" class="fa-solid fa-plus"></i>
+    //     </div>
+    //     <h3>Total : 5 X 700$</h3>
+    //     <button class="remove-product">Remove</button>
+    //   </div>
+    // </div>
+    // `
+}
+
+const seeDetails = () => {
+    console.log("see details")
+}
+
+let incrementBtn = (id) => {
+    let selectedProductId = id;
+    let search = basket.find(product => product.id === selectedProductId.id);
+    if (search === undefined) {
+        basket.push({ id: selectedProductId.id, item: 1 })
+    } else {
+        search.item++;
+    }
+    update(selectedItem.id)
+    localStorage.setItem("data", JSON.stringify(basket));
+};
 
 function hide(x) {
     if (x) {
