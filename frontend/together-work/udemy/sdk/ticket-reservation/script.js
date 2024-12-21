3af9a8d05d17a2ae6440bf4764c2650602c79750
1yavuz <1yavuzahmet@gmail.com>
2024-12-21 21:25:52 +0300

    selected seat save to localStorge

--- frontend/together-work/udemy/sdk/ticket-reservation/script.js
+++ frontend/together-work/udemy/sdk/ticket-reservation/script.js
@@ -1,7 +1,8 @@
 const container = document.querySelector('.container');
 const count = document.querySelector(".count");
 const amount = document.querySelector(".amount");
-const select = document.querySelector("#movie")
+const select = document.querySelector("#movie");
+const seats = document.querySelectorAll(".seat:not(.reserved)");
 
 container.addEventListener("click", function (e) {
     if (e.target.classList.contains("seat")
@@ -17,8 +18,28 @@ select.addEventListener("change", function (e) {
 })
 
 function calculate() {
-    let selectedSeatCount = container.querySelectorAll(".seat.selected").length
-    count.textContent = selectedSeatCount
-    let price = select.value;
-    amount.innerText = selectedSeatCount * price
+    const selectedSeats = container.querySelectorAll('.seat.selected');
+    const selectedSeatArr = [];
+    const seatArr = [];
+    selectedSeats.forEach(seat => {
+        selectedSeatArr.push(seat);
+    });
+    seats.forEach(seat => {
+        seatArr.push(seat);
+    });
+
+    let selectedSeatIndex = selectedSeatArr.map(seat => {
+        return seatArr.indexOf(seat)
+    })
+    console.log(selectedSeatIndex)
+    let selectedSeatCount = selectedSeats.length
+    count.textContent = selectedSeatCount;
+    amount.innerText = selectedSeatCount * select.value;
+
+    saveToLocalStorge(selectedSeatIndex);
+}
+
+function saveToLocalStorge(index) {
+    localStorage.setItem("selectedSeat", JSON.stringify(index));
+    localStorage.setItem("selectedMovieIndex", JSON.stringify(select.selectedIndex)); // Burada selectedIndex kullanılmalı
 }
